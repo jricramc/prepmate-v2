adaeddbfc33b573ac64b610ead9b6327ca4d68dd
jricramc <jricramc@mit.edu>
2023-12-11 22:24:46 -0500

    new changes

---
+++ src/app/Cookies/page.js
@@ -0,0 +1,6 @@
+export default function Cookies() {
+    return(
+    <>
+    </>
+    )
+}
---
+++ src/app/Login/page.js
@@ -0,0 +1,109 @@
+export default function LoginPage () {
+  return (
+    <>
+    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <img height="auto" width="auto"
+            className="mx-auto"
+            src="redPrepMate.png"
+            alt="PrepMate"
+          />
+          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+          <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 rounded border-gray-300 text-primary focus:ring-primary"
+                  />
+                  <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-gray-900">
+                    Remember me
+                  </label>
+                </div>
+
+                <div className="text-sm leading-6">
+                  <a href="#" className="font-semibold text-primary hover:text-red-800">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full items-center justify-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-200"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+
+            <div>
+              <div className="relative mt-10">
+                <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                  <div className="w-full border-t border-gray-200" />
+                </div>
+                <div className="relative flex justify-center text-sm font-medium leading-6">
+                  <span className="bg-white px-6 text-gray-900">Or continue with</span>
+                </div>
+              </div>
+              <div class="flex pt-4">
+                <button class="flex mx-auto px-4 py-2 border gap-2 border-slate-200 rounded-lg text-black transition duration-150">
+                    <img class="w-6 h-6" src="google-color.svg" loading="lazy" alt="google logo"/>
+                    <span>Login with Google</span>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a href="#" className="font-semibold leading-6 text-primary hover:text-red-800">
+              Sign Up Now
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
