582c0ac854b7edf3d55ed5c0817e3306d851a1ae
jricramc <jricramc@mit.edu>
2023-12-14 11:56:22 -0500

    Update page.js

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -263,17 +263,17 @@ export default function ContactUs() {
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
         <p className="mt-2 text-lg leading-8 text-gray-600">
-          If you have any comments, questions, or suggestions, please contact us using the form below. We will get back to you as soon as possible.
+          If you have any comments, questions, or suggestions, please contact us using the email below. We will get back to you as soon as possible.
         </p>
       </div>
       <div className="mt-10">
         <a
-          href="mailto:founder@prepmate.shop"
-          target="_blank"
-          rel="noopener noreferrer"
+          // href="mailto:founder@prepmate.shop"
+          // target="_blank"
+          // rel="noopener noreferrer"
           className="block w-full rounded-md bg-primary px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
         >
-          Contact Us via Email
+          founder@prepmate.shop
         </a>
       </div>
     </div>
